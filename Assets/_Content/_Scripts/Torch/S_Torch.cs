a2de66ed6ffe49d003bb443e58a148048ce3432c
xionck <xarc35.game@gmail.com>
2024-09-11 16:33:23 +0200

    Light lit to unlit working but not the opposite / DebugLog bugged

---
+++ Assets/_Content/_Scripts/Torch/S_Torch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Torch : MonoBehaviour
+{
+    [SerializeField] private Light torchLight;
+    [SerializeField] private int torchIntensity;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void ChangeLightState()
+    {
+        if (torchLight.GetComponent<Light>().enabled = true)
+        {
+            torchLight.GetComponent<Light>().enabled = false;
+        }  
+        else
+        {
+            torchLight.GetComponent<Light>().enabled = true;
+        }
+        return;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("e"))
+        {
+            ChangeLightState();
+        }
+    }
+}
