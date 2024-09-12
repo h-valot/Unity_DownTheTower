3ccb4418bfd1c9b0793966c2002a01437d94f805
Noah COUPEY <n_coupey@stu-bellecour.fr>
2024-09-12 08:09:32 +0200

    Sound System Skeleton

---
+++ Assets/_Content/_Scripts/Data/RSE/Sound/RSE_PlaySound.cs
@@ -0,0 +1,5 @@
+using RuntimeScriptables;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_PlaySound", menuName = "RSE/Sound/PlaySound")]
+public class RSE_PlaySound : RuntimeScriptableEvent<Sound> { }
