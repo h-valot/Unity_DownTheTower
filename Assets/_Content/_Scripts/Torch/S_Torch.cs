d1203e033998591cfc08cca2e9f2572d995f7031
xionck <xarc35.game@gmail.com>
2024-09-12 11:04:23 +0200

    throw torch logic, need to make throw() in player script

--- Assets/_Content/_Scripts/Torch/S_Torch.cs
+++ Assets/_Content/_Scripts/Torch/S_Torch.cs
@@ -1,29 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class S_Torch : MonoBehaviour
 {
     [SerializeField] private Light torchLight;
     [SerializeField] private int torchIntensity;
+    private Camera playerCam;
+    [SerializeField] private GameObject ThrowTorchPrefab;
+    private Rigidbody torchRigidbody;
         
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCam = Camera.main;
+        torchRigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
-    void ChangeLightState()
+    void ChangeLightState() // Function to call to lit or unlit the torch
     {
-        if (torchLight.GetComponent<Light>().enabled == true)
+        if (torchLight.GetComponent<Light>().enabled == true) // Si lit, unlit la torche
         {
             torchLight.GetComponent<Light>().enabled = false;
         }  
         else
         {
-            torchLight.GetComponent<Light>().enabled = true;
+            torchLight.GetComponent<Light>().enabled = true; // Si Unlit, lit la torche
         }
         return;
+    }
+
+    void ThrowTorch(Vector3 throwForward)
+    {
+        Ray r = playerCam.ScreenPointToRay(Input.mousePosition);
+
+        Vector3 dir = r.GetPoint(1) - r.GetPoint(0);
+        torchRigidbody.constraints = RigidbodyConstraints.None;
+        ThrowTorchPrefab.GetComponent<Rigidbody>().velocity = throwForward * 5;
+        
     }
     // Update is called once per frame
     void Update()
@@ -31,6 +46,11 @@ public class S_Torch : MonoBehaviour
         if (Input.GetKeyDown("e"))
         {
             ChangeLightState();
+        }
+
+        if (Input.GetKeyDown("g"))
+        {
+            
         }
     }
 }
