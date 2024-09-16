f994983da812617792f7b00ac3ca94638ddbc112
xionck <xarc35.game@gmail.com>
2024-09-16 10:47:13 +0200

    lit and unlit mats + logic, rework logic with _isActive + rigidbody Serialize instead of GetComponent

--- Assets/_Content/_Scripts/Torch/S_Torch.cs
+++ Assets/_Content/_Scripts/Torch/S_Torch.cs
@@ -7,32 +7,39 @@ public class S_Torch : MonoBehaviour
 {
     [Header("Internal References")]
     [SerializeField] private Light _torchLight;
-    [SerializeField] private TorchConfig _torchConfig; 
+    [SerializeField] private Rigidbody _torchRigidbody;
+    [SerializeField] private MeshRenderer _litBody;
+    [SerializeField] private Material _litMaterial;
+    [SerializeField] private Material _unlitMaterial;
+    [SerializeField] private TorchConfig _torchConfig;
 
     // ----- PRIVATE VARIABLES -----
-    private Rigidbody _torchRigidbody;
-    private bool _canThrow = true;
+    private bool _isActive;
+
+  
 
     // Start is called before the first frame update²
     void Start()
     {
         _torchLight.intensity = _torchConfig.torchIntensity;
-        _torchRigidbody = gameObject.GetComponent<Rigidbody>();
         _torchRigidbody.constraints = RigidbodyConstraints.FreezeAll;
-
+        _isActive = true;
     }
 
     public void ChangeLightState() // Function to call to lit or unlit the torch
     {
-        if (_canThrow == true)
+        if (_isActive == true)
         {
             if (_torchLight.enabled == true) // Si lit, unlit la torche
             {
                 _torchLight.enabled = false;
+                _litBody.material = _unlitMaterial;
+                
             }
             else
             {
                 _torchLight.enabled = true; // Si Unlit, lit la torche
+                _litBody.material = _litMaterial;
             }
             return;
         }
@@ -40,26 +47,23 @@ public class S_Torch : MonoBehaviour
 
     public void ThrowTorch(Vector3 _throwForward)
     {
-        if (_canThrow == true)
+        if (_isActive == true)
         {
-            gameObject.transform.parent = null;
-            _torchRigidbody.constraints = RigidbodyConstraints.None;
-            gameObject.GetComponent<Rigidbody>().velocity = _throwForward * 10;
-            _canThrow = false;
+            if (_torchConfig.canThrow == true)
+            {
+                gameObject.transform.parent = null;
+                _torchRigidbody.constraints = RigidbodyConstraints.None;
+                gameObject.GetComponent<Rigidbody>().velocity = _throwForward * 10;
+                _isActive = false;
 
-            StartCoroutine(WaitAndDestroyTorch(_torchConfig.litDuration));
+                StartCoroutine(WaitAndDestroyTorch(_torchConfig.litDuration));
+            }
         }
-
     }
 
     IEnumerator WaitAndDestroyTorch(int _time)
     {
         yield return new WaitForSeconds(_time);
         Destroy(gameObject);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
--- Assets/_Content/_Scripts/Torch/S_TorchConfig.cs
+++ Assets/_Content/_Scripts/Torch/S_TorchConfig.cs
@@ -7,8 +7,17 @@ public class TorchConfig : ScriptableObject
     [Tooltip("Intensity of the light")]
     public int torchIntensity;
 
+    [Tooltip("Time to craft the torch")]
+    public int timeToCraft;
+
     [Tooltip("Lit duration when on ground")]
     public int litDuration;
+
+    [Tooltip("Time to lit the torch")]
+    public int timeToLit;
+
+    [Tooltip("Time to unlit the torch")]
+    public int timeToUnlit;
 
     [Tooltip("Can throw the torch when handled")]
     public bool canThrow;
