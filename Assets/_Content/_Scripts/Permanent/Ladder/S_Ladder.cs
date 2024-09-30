ae20ec32432784f4ce01267281770bbbf8c27432
Kamillekaz <laborderie.camille@gmail.com>
2024-09-29 19:10:06 -0700

    Rework ladder logic

--- Assets/_Content/_Scripts/Permanent/Ladder/S_Ladder.cs
+++ Assets/_Content/_Scripts/Permanent/Ladder/S_Ladder.cs
@@ -2,26 +2,131 @@ using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ladder : MonoBehaviour
+public class Ladder : Permanent
 {
 	[Header("Internal references")]
     [SerializeField] private GameObject _ladderMesh;
+    [SerializeField] private GameObject _previewLadder;
+    [SerializeField] private MeshRenderer _previewLadderRenderer;
     [SerializeField] private GameObject _TPTriggerTop;
     [SerializeField] private GameObject _TPTriggerBottom;
 	[SerializeField] private GameObject _tippyTop;
 
-	[Header("Scriptable references")]
+    [Header("Scriptable references")]
 	[SerializeField] private LadderConfig _ladderConfig;
 
     private Vector3 _bottomPos;
     private Vector3 _topHitPos;
 
-    public void Initialize()
+    public override void InitializePreview()
+    {
+        _previewLadder.SetActive(true);
+        _previewLadder.transform.localScale = new Vector3(
+            _previewLadder.transform.localScale.x,
+            _ladderConfig.maxHeight / 2,
+            _previewLadder.transform.localScale.z
+        );
+        _previewLadder.transform.position = new Vector3(
+            _previewLadder.transform.position.x,
+            transform.position.y + (_ladderConfig.maxHeight / 2),
+            _previewLadder.transform.position.z
+        );
+        _previewLadder.transform.rotation = Quaternion.identity;
+    }
+
+    //Call in Late Update
+    public override void PreviewThrow(Transform _cameraTransform)
+    {
+        if (Physics.Raycast(_cameraTransform.position, GetPositionRayDirection(_cameraTransform), out var hit, _ladderConfig.maxDistFromCamera, ~(_ladderConfig.layersToIgnore)))
+        {
+            if (!_previewLadder.activeSelf) _previewLadder.SetActive(true);
+            _previewLadder.transform.position = hit.point;
+            UpdateColor(IsGroundFlat(hit) && !IsCeiling(hit) && !IsSpaceInFront(hit, _cameraTransform));
+        }
+        else
+        {
+            UpdateColor(false);
+            if (_previewLadder.activeSelf) _previewLadder.SetActive(false);
+        }
+    }
+
+    private Vector3 GetPositionRayDirection(Transform _cameraTransform)
+    {
+        Vector3 offsetRay = Quaternion.AngleAxis(_ladderConfig.cameraOffset, _cameraTransform.right) * _cameraTransform.forward;
+        float angleDifference = Vector3.SignedAngle(new Vector3(_cameraTransform.forward.x, 0, _cameraTransform.forward.z).normalized, offsetRay.normalized, _cameraTransform.right);
+        if (_ladderConfig.maxCameraDownwardClamp < angleDifference)
+        {
+            offsetRay = Quaternion.AngleAxis(_ladderConfig.cameraOffset - (angleDifference - _ladderConfig.maxCameraDownwardClamp) * 0.5f, _cameraTransform.right).normalized * _cameraTransform.forward;
+        }
+
+        return offsetRay;
+    }
+
+    private bool IsGroundFlat(RaycastHit hit)
+    {
+        float product = Vector3.Dot(hit.normal, new Vector3(0, 1, 0));
+        return (product >= _ladderConfig.maxGroundAngle);
+    }
+
+
+    private bool IsCeiling(RaycastHit hit)
+    {
+        //Check that there is enough room above current position
+        return Physics.Raycast(hit.point + new Vector3(0, 0.25f, 0), Vector3.up, _ladderConfig.maxHeight - 0.25f);
+    }
+
+    private bool IsSpaceInFront(RaycastHit hit, Transform _cameraTransform)
+    {
+        //Check that there is a little room in front of ladder
+        return Physics.Raycast(hit.point + new Vector3(0, 0.25f, 0),
+            Vector3.Normalize(new Vector3(_cameraTransform.forward.x, 0, _cameraTransform.forward.z)), _ladderConfig.minDistanceFromWall);
+    }
+
+    private void UpdateColor(bool _isDeployable)
+    {
+        if (!_isDeployable)
+        {
+            if (_previewLadderRenderer.material.GetFloat("_colorSwitch") != 1f) _previewLadderRenderer.material.SetFloat("_colorSwitch", 1f);
+        }
+        else
+        {
+            if (_previewLadderRenderer.material.GetFloat("_colorSwitch") != 0f) _previewLadderRenderer.material.SetFloat("_colorSwitch", 0f);
+        }
+    }
+
+    public override bool Throw(Transform _cameraTransform)
+    {
+        _previewLadder.SetActive(false);
+
+        if (Physics.Raycast(_cameraTransform.position, GetPositionRayDirection(_cameraTransform), out var hit, _ladderConfig.maxDistFromCamera, ~(_ladderConfig.layersToIgnore)))
+        {
+            if (IsGroundFlat(hit) && !IsCeiling(hit) && !IsSpaceInFront(hit, _cameraTransform))
+            {
+                transform.SetParent(null, true);
+                Deploy(_cameraTransform, hit.point);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Deploy(Transform _cameraTransform, Vector3 _deployPosition)
 	{
-		_bottomPos = transform.position;
-        _tippyTop.transform.position = new Vector3(_bottomPos.x, _bottomPos.y + _ladderConfig.maxHeight, _bottomPos.z);
-        _ladderMesh.transform.DOScaleY(_ladderConfig.maxHeight / 2, 1);
-        _ladderMesh.transform.DOMoveY(transform.position.y + (_ladderConfig.maxHeight / 2), 1).OnComplete(Falling);
+        transform.eulerAngles = new Vector3(0, _cameraTransform.rotation.eulerAngles.y, 0);
+        transform.DOJump(_deployPosition, 1f, 0, 0.3f).OnComplete(() =>
+        {
+            _bottomPos = transform.position;
+            _tippyTop.transform.position = new Vector3(_bottomPos.x, _bottomPos.y + _ladderConfig.maxHeight, _bottomPos.z);
+            _ladderMesh.transform.DOScaleY(_ladderConfig.maxHeight / 2, 1);
+            _ladderMesh.transform.DOMoveY(transform.position.y + (_ladderConfig.maxHeight / 2), 1).OnComplete(Falling);
+        });
 	}
 
     private void Falling()
@@ -59,8 +164,8 @@ public class Ladder : MonoBehaviour
         _TPTriggerBottom.SetActive(true);
 
         // pass on values to trigger zones (origin, destination, isBottom)
-        _TPTriggerTop.GetComponent<LadderTP>().SetVariables(_topHitPos, _bottomPos - transform.forward, false);
-        _TPTriggerBottom.GetComponent<LadderTP>().SetVariables(_bottomPos, PickBestTeleportPoint(), true);
+        _TPTriggerTop.GetComponent<LadderTP>().SetVariables(_topHitPos, _bottomPos - transform.forward);
+        _TPTriggerBottom.GetComponent<LadderTP>().SetVariables(_bottomPos, PickBestTeleportPoint());
     }
     
     private Vector3 PickBestTeleportPoint()
