a31ad8106d034f7fa7c4f8e0043d0a42296298e1
ncoupey-schl <74413792+ncoupey@users.noreply.github.com>
2024-10-03 17:01:00 +0200

    Fixed GameConfig error

--- Assets/_Content/_Scripts/World/S_GameConfig.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameConfig", menuName = "Configs/Game")]
-public class GameConfig : ScriptableObject
-{
-	[Header("Debug")]
-	public bool enableConsoleLogging;
-}	
