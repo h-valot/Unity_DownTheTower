4f797b8132452c99f83d210258fa0c49c8ec93b5
xionck <xarc35.game@gmail.com>
2024-10-08 17:04:19 +0200

    Breathing mushroom

--- Assets/_Content/_Scripts/Enviro/S_ExplosiveMushroom.cs
+++ Assets/_Content/_Scripts/Enviro/S_ExplosiveMushroom.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ExplosiveMushroom : MonoBehaviour
 {
+    [SerializeField] private GameObject _mushroom;
+    private float _timeScaling;
+    private float _timeScaleX = 1;
+
+    public void Explode()
+    {
+        _timeScaleX = Mathf.PingPong(_timeScaling, 1);
+        _mushroom.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+    }
+
+    public void Refilled()
+    {
+        _timeScaleX = 1;
+    }
 
     void Update()
     {
-        
+        _timeScaling = Time.time/10;
+        _mushroom.transform.localScale = new Vector3 (_timeScaleX, Mathf.PingPong(_timeScaling, 0.2f) + 0.8f, Mathf.PingPong(_timeScaling, 0.2f) + 0.8f);
     }
 }
