b66126ca9d3e9d51576e0b4a3464fdb4028db26d
kamillekaz <laborderie.camille@gmail.com>
2024-10-28 11:09:12 +0100

    Character controller working (no edge sorting)

--- Assets/_Content/_Scripts/Data/Inputs/RSE_Sprint.cs
+++
@@ -1,5 +0,0 @@
-using RuntimeScriptables;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "RSE_Sprint", menuName = "RSE/Inputs/Sprint")]
-public class RSE_Sprint : RuntimeScriptableEvent<bool> {}
