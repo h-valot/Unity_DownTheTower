96c8478818154794bd5da0b1d1ddd59221562f6b
xionck <xarc35.game@gmail.com>
2025-01-14 14:31:47 +0100

    Clean print after merge

--- Assets/_Content/_Scripts/Character/S_AnimatorManager.cs
+++ Assets/_Content/_Scripts/Character/S_AnimatorManager.cs
@@ -24,7 +24,6 @@ public class AnimatorManager : MonoBehaviour
         m_animator.SetFloat(m_moveSpeedHash, Mathf.Abs(m_characterMotor.Rigidbody.velocity.z) + Mathf.Abs(m_characterMotor.Rigidbody.velocity.x));
         m_animator.SetBool(m_isJumpingHash, m_characterMotor.m_hasJumped);
         m_animator.SetBool(m_isGroundedHash, m_characterMotor.m_isGrounded);
-        print(m_currentState.ToString());
     }
 
     private void DetermineState()
