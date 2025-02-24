9a2f4a6625b1b80b5bcacdf7a54c78314ff4012f
hvalot-schl <109879255+h-valot@users.noreply.github.com>
2025-02-24 11:23:19 +0100

    Fix camera offset at start

--- Assets/_Content/_Scripts/Manager/S_InputManager.cs
+++ Assets/_Content/_Scripts/Manager/S_InputManager.cs
@@ -53,6 +53,7 @@ public class InputManager : MonoBehaviour
 		// Reset values
 		m_rsoCraftInputLocked.value = false;
 		m_rsoRecycleInputLocked.value = false;
+		m_rseLook.Call(Vector2.zero);
 	}
 
 	private void Start()
