e16772acfed47eaa0c392fd91951b161ad2029cd
hvalot-schl <109879255+h-valot@users.noreply.github.com>
2025-02-26 12:19:34 +0100

    Enhance camera behavior

--- Assets/_Content/_Scripts/Character/S_CameraMotor.cs
+++ Assets/_Content/_Scripts/Character/S_CameraMotor.cs
@@ -1,13 +1,14 @@
 using Cinemachine;
-using Mono.Cecil.Cil;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class CameraMotor : MonoBehaviour
 {
+	#region REFERENCES
+
 	[Title("External references")]
 	[SerializeField] private CinemachineVirtualCamera m_aimingCamera;
-	[SerializeField] private CinemachineVirtualCamera m_thirdPersonCamera;
+	[SerializeField] private CinemachineVirtualCamera m_3rdPersonCamera;
 
 	[FoldoutGroup("Scriptable")][SerializeField] private SSO_Camera m_ssoCamera;
 
@@ -23,11 +24,38 @@ public class CameraMotor : MonoBehaviour
 	[FoldoutGroup("Scriptable")][SerializeField] private RSO_CameraTransform m_rsoCameraTransform;
 	[FoldoutGroup("Scriptable")][SerializeField] private RSO_InputsLocked m_rsoInputsLocked;
 
-	// - Private variables -
+	#endregion
+
+	#region VARIABLES
+
+	// - Movement -
 	private Vector2 m_lookInput;
 	private float m_cinemachineTargetYaw;
 	public float m_cinemachineTargetPitch;
-	private Transform m_cameraTarget;
+	private Transform m_lookAt;
+
+	// - Custom effect value targets - 
+	private Cinemachine3rdPersonFollow m_3rdPersonFollow;
+
+	// Look at 
+	private Vector3 m_targetLookAt;
+	private Vector3 m_LookAtSmoothVelocity;
+	private Vector3 m_defaultLookAtPosition;
+
+	// Camera Distance
+	private float m_targetCameraDistance;
+
+	// Shoulder Offset Z
+	private float m_targetShoulderOffsetZ;
+	private const float k_defaultShoulderOffsetZ = 0.0f;
+
+	// Camera Side
+	private float m_targetCameraSide;
+	private const float k_defaultCameraSide = 0.5f;
+
+	#endregion 
+	
+	#region MONOBEHAVIOR
 
 	private void OnEnable()
 	{
@@ -59,17 +87,24 @@ public class CameraMotor : MonoBehaviour
 		m_rsoCameraTransform.value = transform;
 	}
 
+	#endregion 
+
+	#region MOTOR
+
+
 	public void Initialize(Transform aimingLookAt, Transform cameraTarget, Quaternion startRotation)
 	{
-		m_cameraTarget = cameraTarget;
-		m_defaultTargetLocalPosition = cameraTarget.localPosition;
+		m_lookAt = cameraTarget;
+		m_defaultLookAtPosition = cameraTarget.localPosition;
 		m_aimingCamera.Follow = cameraTarget;
 		m_aimingCamera.LookAt = aimingLookAt;
-		m_thirdPersonCamera.Follow = cameraTarget;
-		m_thirdPersonCamera.LookAt = cameraTarget;
+		m_3rdPersonCamera.Follow = cameraTarget;
+		m_3rdPersonCamera.LookAt = cameraTarget;
 
-		m_targetDistance = m_ssoCamera.DefaultDistance;
-		m_3rdPersonFollow = m_thirdPersonCamera.GetCinemachineComponent(CinemachineCore.Stage.Body) as Cinemachine3rdPersonFollow;
+		m_targetCameraDistance = m_ssoCamera.DefaultCameraDistance;
+		m_3rdPersonFollow = m_3rdPersonCamera.GetCinemachineComponent(CinemachineCore.Stage.Body) as Cinemachine3rdPersonFollow;
+		m_3rdPersonFollow.DampingFromCollision = m_ssoCamera.DampingFromCollision;
+		m_3rdPersonFollow.ShoulderOffset.y = m_ssoCamera.ShoulderOffsetY;
 
 		m_rsoCameraForward.value = new Vector3(transform.forward.x, 0, transform.forward.z);
 		m_rsoCameraRight.value = new Vector3(transform.right.x, 0, transform.right.z);
@@ -89,16 +124,8 @@ public class CameraMotor : MonoBehaviour
 		if (m_rsoCharacterDeath.value) return;
 
 		// Cinemachine will follow this target
-		m_cameraTarget.rotation = Quaternion.Euler(m_cinemachineTargetPitch, m_cinemachineTargetYaw, 0.0f);
+		m_lookAt.rotation = Quaternion.Euler(m_cinemachineTargetPitch, m_cinemachineTargetYaw, 0.0f);
 	}
-
-	private Cinemachine3rdPersonFollow m_3rdPersonFollow;
-
-	private float m_targetDistance;
-	private Vector3 m_targetPosition;
-
-	private Vector3 m_cameraTargetSmoothVelocity;
-	private Vector3 m_defaultTargetLocalPosition;
 
 	private void HandleSuspended()
 	{
@@ -107,63 +134,56 @@ public class CameraMotor : MonoBehaviour
 		{
 			if (m_rsoCharacterState.value == BehaviorState.ROPE)
 			{
-				m_targetDistance = m_ssoCamera.SuspendedDistance;
-				m_targetPosition = m_defaultTargetLocalPosition + Vector3.down * m_ssoCamera.SuspendedTargetLocalOffset;
+				m_targetCameraDistance = m_ssoCamera.SuspendedCameraDistance;
+				m_targetLookAt = m_defaultLookAtPosition + Vector3.down * m_ssoCamera.SuspendedLookAtOffsetY;
 			}
 			else
 			{
-				m_targetDistance = m_ssoCamera.DefaultDistance;
-				m_targetPosition = m_defaultTargetLocalPosition;
+				m_targetCameraDistance = m_ssoCamera.DefaultCameraDistance;
+				m_targetLookAt = m_defaultLookAtPosition;
 			}
 		}
 
 		// Lerp towards target distance
 		m_3rdPersonFollow.CameraDistance = Mathf.Lerp(
 			m_3rdPersonFollow.CameraDistance, 
-			m_targetDistance, 
-			Time.deltaTime * m_ssoCamera.DistanceTransition
+			m_targetCameraDistance, 
+			Time.deltaTime * m_ssoCamera.TransitionCameraDistance
 		);
 
 		// Lerp the camera look at target towards target position
-		m_cameraTarget.localPosition = Vector3.SmoothDamp(
-			m_cameraTarget.localPosition,
-			m_targetPosition,
-			ref m_cameraTargetSmoothVelocity,
-			Time.deltaTime * m_ssoCamera.TargetTransition
+		m_lookAt.localPosition = Vector3.SmoothDamp(
+			m_lookAt.localPosition,
+			m_targetLookAt,
+			ref m_LookAtSmoothVelocity,
+			Time.deltaTime * m_ssoCamera.TransitionLookAt
 		);
 	}
-
-	private float m_defaultShoulderOffsetZ = 0.0f;
-	private float m_targetShoulderOffsetZ;
-	private float m_locomotionShoulderOffsetZ = 1f;
-	private float m_thresholdShouldOffsetZ = 55f;
-
-	private float m_defaultCameraSide = 0.5f;
-	private float m_targetCameraSide;
-	private float m_locomotionCameraSide = 0.5f;
-	private float m_thresholdCameraSide = -10f;
 
 	public void HandleLocomotion()
 	{
 		// Choose targets 
 		if (m_rsoCameraStyle.value == CameraStyle.BASIC)
 		{
-			m_targetShoulderOffsetZ = m_defaultShoulderOffsetZ;
-			m_targetCameraSide = m_defaultCameraSide;
+			m_targetShoulderOffsetZ = k_defaultShoulderOffsetZ;
+			m_targetCameraSide = k_defaultCameraSide;
 
 			// Override those targets
 			if (m_rsoCharacterState.value == BehaviorState.LOCOMOTION)
 			{
 				// Camera is looking downwards ---- 70 = down
-				if (m_cinemachineTargetPitch >= m_thresholdShouldOffsetZ)
+				if (m_cinemachineTargetPitch >= m_ssoCamera.ThresholdShouldOffsetZ)
 				{
-					m_targetShoulderOffsetZ = m_locomotionShoulderOffsetZ;
+					m_targetShoulderOffsetZ = m_ssoCamera.LocomotionShoulderOffsetZ;
 				}
 
 				// Camera is looking upwards ---- -60 = up
-				if (m_cinemachineTargetPitch <= m_thresholdCameraSide)
+				if (m_cinemachineTargetPitch <= m_ssoCamera.ThresholdCameraSide)
 				{
-					m_targetCameraSide = m_defaultCameraSide + m_locomotionCameraSide * Matha.Remap(m_thresholdCameraSide, m_ssoCamera.BottomClamp, 0f, 1f, m_cinemachineTargetPitch);
+					m_targetCameraSide = 
+						k_defaultCameraSide 
+						+ m_ssoCamera.LocomotionCameraSide 
+						* Matha.Remap(m_ssoCamera.ThresholdCameraSide, m_ssoCamera.BottomClamp, 0f, 1f, m_cinemachineTargetPitch);
 				}
 			}
 		}
@@ -172,23 +192,23 @@ public class CameraMotor : MonoBehaviour
 		m_3rdPersonFollow.ShoulderOffset.z = Mathf.Lerp(
 			m_3rdPersonFollow.ShoulderOffset.z,
 			m_targetShoulderOffsetZ,
-			Time.deltaTime * m_ssoCamera.DistanceTransition
+			Time.deltaTime * m_ssoCamera.TransitionShoulderOffsetZ
 		);
 
 		// Lerp towards target camera side
 		m_3rdPersonFollow.CameraSide = Mathf.Lerp(
 			m_3rdPersonFollow.CameraSide,
 			m_targetCameraSide,
-			Time.deltaTime * 5
+			Time.deltaTime * m_ssoCamera.TransitionCameraSide
 		);
 	}
 
 	public void SwitchStyle()
 	{
 		m_aimingCamera.gameObject.SetActive(false);
-		m_thirdPersonCamera.gameObject.SetActive(false);
+		m_3rdPersonCamera.gameObject.SetActive(false);
 
-		if (m_rsoCameraStyle.value == CameraStyle.BASIC) m_thirdPersonCamera.gameObject.SetActive(true);
+		if (m_rsoCameraStyle.value == CameraStyle.BASIC) m_3rdPersonCamera.gameObject.SetActive(true);
 		if (m_rsoCameraStyle.value == CameraStyle.AIMING) m_aimingCamera.gameObject.SetActive(true);
 	}
 
@@ -221,6 +241,8 @@ public class CameraMotor : MonoBehaviour
 
 	private void FreeCamera()
 	{
-		m_cameraTarget.transform.parent = null;
+		m_lookAt.transform.parent = null;
 	}
+
+	#endregion
 }
--- Assets/_Content/_Scripts/Static Scriptable/SSO_Camera.cs
+++ Assets/_Content/_Scripts/Static Scriptable/SSO_Camera.cs
@@ -4,38 +4,108 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SSO_Camera", menuName = "Static Scriptable/Camera")]
 public class SSO_Camera : ScriptableObject
 {
-	[InfoBox("Starting style of the camera. BASIC: default third person camera. AIMING: third person camera where the character always face the camera forward direction.", InfoMessageType.None)]
+	#region GENERAL SETTINGS
+
+	[FoldoutGroup("General settings")]
+	[InfoBox("Starting style of the camera. BASIC: Default 3rd Person Camera. AIMING: 3rd Person Camera where the character always face the camera forward direction.", InfoMessageType.None)]
 	public CameraStyle StartingStyle;
 
+	[FoldoutGroup("General settings")]
 	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
 	[InfoBox("Maximum angle of the camera pitch can move up to.", InfoMessageType.None)]
 	public float TopClamp;
 
+	[FoldoutGroup("General settings")]
 	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
 	[InfoBox("Minimum angle of the camera pitch can move down to.", InfoMessageType.None)]
 	public float BottomClamp;
 
+	[FoldoutGroup("General settings")]
 	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
 	[InfoBox("Speed value of the lerp function that orient the character graphics in the direction of the movement.", InfoMessageType.None)]
 	public float RotationSpeed;
 
-	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
-	[InfoBox("Distance between the 3rd person camera and the character while not suspended on rope.", InfoMessageType.None)]
-	public float DefaultDistance;
 
+	#endregion
+
+	#region GLOBAL PARAMETERS
+
+	[FoldoutGroup("Global parameters")]
+	[PropertyRange(0f, 10f)]
+	[InfoBox("Lerp when the camera zoom out back to its maximum distance.", InfoMessageType.None)]
+	public float DampingFromCollision;
+
+	[FoldoutGroup("Global parameters")]
+	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
+	[PropertyRange(0.0f, 0.5f)]
+	[InfoBox("Value added to the Cinemachine 3rd Person Camera on the Shoulder Offset Y when the distance between the character and the camera is below a threshold. (Default = 0.0f)", InfoMessageType.None)]
+	public float ShoulderOffsetY;
+
+	#endregion
+
+	#region SUSPENDED BEHAVIOR
+
+	[Title("Camera Distance")]
+	[FoldoutGroup("Suspended behavior")]
+	[InfoBox("Distance between the 3rd person camera and the character while not suspended on rope.", InfoMessageType.None)]
+	public float DefaultCameraDistance;
+
+	[FoldoutGroup("Suspended behavior")]
 	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
 	[InfoBox("Distance between the 3rd person camera and the character while suspended on rope.", InfoMessageType.None)]
-	public float SuspendedDistance;
+	public float SuspendedCameraDistance;
 
+	[FoldoutGroup("Suspended behavior")]
 	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
-	[InfoBox("Duration of the lerp transition between default and suspended camera distance.", InfoMessageType.None)]
-	public float DistanceTransition;
+	[InfoBox("Duration of the lerp transition between default and suspended camera distance. (Mathf.Lerp() so the smaller, the quicker)", InfoMessageType.None)]
+	public float TransitionCameraDistance;
 
-	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
-	[InfoBox("Duration of the lerp transition between default and suspended camera target local Y offset.", InfoMessageType.None)]
-	public float TargetTransition;
 
-	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
+	[Title("Look at local offset Y")]
+	[FoldoutGroup("Suspended behavior")]
 	[InfoBox("Offset applied to the camera target on the y-axis.", InfoMessageType.None)]
-	public float SuspendedTargetLocalOffset;
+	public float SuspendedLookAtOffsetY;
+
+	[FoldoutGroup("Suspended behavior")]
+	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
+	[InfoBox("Duration of the lerp transition between default and suspended camera target local Y offset. (Vector3.SmoothDamp() so the smaller, the slower)", InfoMessageType.None)]
+	public float TransitionLookAt;
+
+	#endregion
+
+	#region LOCOMOTION BEHAVIOR
+
+	[Title("Shoulder Offset Z")]
+	[FoldoutGroup("Locomotion behavior")]
+	[InfoBox("Value applied as an override to the Cinemachine 3rd Person Camera on the Shoulder Offset Z when the character is looking down. (Default = 0.0f)", InfoMessageType.None)]
+	public float LocomotionShoulderOffsetZ = 1f;
+
+	[FoldoutGroup("Locomotion behavior")]
+	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
+	[InfoBox("Threshold of the camera pitch value to apply the custom value to the Shoulder Offset Z.", InfoMessageType.None)]
+	public float ThresholdShouldOffsetZ = 55f;
+
+	[FoldoutGroup("Locomotion behavior")]
+	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
+	[InfoBox("Durationg of the lerp transition between default and custom Shoulder Offset Z. (Mathf.Lerp() so the smaller, the quicker)", InfoMessageType.None)]
+	public float TransitionShoulderOffsetZ;
+
+
+	[Title("Camera Side")]
+	[FoldoutGroup("Locomotion behavior")]
+	[PropertyRange(0.0f, 0.5f)]
+	[InfoBox("Value added to the Cinemachine 3rd Person Camera on the Camera Side when the character is looking up. (Default = 0.5f)", InfoMessageType.None)]
+	public float LocomotionCameraSide = 0.5f;
+
+	[FoldoutGroup("Locomotion behavior")]
+	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
+	[InfoBox("Threshold of the camera pitch value to start gradually adding the custom Camera Side value.", InfoMessageType.None)]
+	public float ThresholdCameraSide = -10f;
+
+	[FoldoutGroup("Locomotion behavior")]
+	[PropertySpace(SpaceBefore = 15, SpaceAfter = 0)]
+	[InfoBox("Duration of the lerp transition between default and custom Camera Side. (Mathf.Lerp() so the smaller, the quicker)", InfoMessageType.None)]
+	public float TransitionCameraSide;
+
+	#endregion
 }
