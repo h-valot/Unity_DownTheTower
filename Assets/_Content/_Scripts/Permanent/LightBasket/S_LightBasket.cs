aef45bca4cc154dd8aa2017d7e632ae9a94f7bc0
kamillekaz <laborderie.camille@gmail.com>
2025-03-19 17:53:36 +0100

    Fix ruen basket of light

--- Assets/_Content/_Scripts/Permanent/LightBasket/S_LightBasket.cs
+++ Assets/_Content/_Scripts/Permanent/LightBasket/S_LightBasket.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
@@ -8,9 +9,29 @@ public class LightBasket : MonoBehaviour
 {
     [SerializeField] private LightReciever _parentDoor;
     [SerializeField] private float _maxVelocitySnap = 1;
+    [SerializeField] private MeshRenderer m_meshRendererBasket;
+    [SerializeField] private float m_activationDuration = 1f;
+    [SerializeField] private float m_activeEmitStrength = 1f;
+    [SerializeField] private float m_deactiveEmitStrength = 1f;
+
+    private MaterialPropertyBlock m_propertyBlockBasket;
+    private float m_currentEmitStrength;
 
     private bool _isFilled = false;
     private List<Rigidbody> torches = new List<Rigidbody>();
+
+    private void Awake()
+    {
+        m_currentEmitStrength = m_deactiveEmitStrength;
+        m_propertyBlockBasket = new MaterialPropertyBlock();
+        m_propertyBlockBasket.SetFloat("_EmitStrength", m_currentEmitStrength);
+        m_meshRendererBasket.SetPropertyBlock(m_propertyBlockBasket);
+    }
+
+    private void OnDisable()
+    {
+        DOTween.Kill(this);
+    }
 
     private void Update()
     {
@@ -46,5 +67,10 @@ public class LightBasket : MonoBehaviour
         _isFilled = true;
         torch.GetComponent<Torch>().DetachAndCancelDeactivation();
         _parentDoor.AddToLightCounter();
+        DOTween.To(() => m_currentEmitStrength, x => m_currentEmitStrength = x, m_activeEmitStrength, 0.5f).SetTarget(this).SetEase(Ease.Linear)
+                            .OnUpdate(() => {
+                                m_propertyBlockBasket.SetFloat("_EmitStrength", m_currentEmitStrength);
+                                m_meshRendererBasket.SetPropertyBlock(m_propertyBlockBasket);
+                            });
     }
 }
